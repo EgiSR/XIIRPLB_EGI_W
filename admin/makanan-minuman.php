552fdd1a382a4d9fa750ef95669355a643917905
EgiSR <egysatmata@gmail.com>
2023-03-13 12:01:52 +0700

    four commit

---
+++ admin/makanan-minuman.php
@@ -0,0 +1,20 @@
+<section>
+  <h5>Halaman Makanan & Minuman</h5>
+  <a href="?url=tambah-spp" class="btn btn-primary">Tambah Menu</a>
+
+  <table class="table table-striped table-bordered">
+    <thead>
+      <tr class="fw-bold">
+        <th>No</th>
+        <th>Nama Masakan</th>
+        <th>Harga</th>
+        <th>Edit</th>
+        <th>Hapus</th>
+      </tr>
+    </thead>
+
+    <tbody>
+
+    </tbody>
+  </table>
+</section>
